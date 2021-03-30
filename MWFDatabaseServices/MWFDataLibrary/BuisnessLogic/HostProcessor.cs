97ca38b8a5fcb57269f85332898d6cac98b008d3
Brian2524 <b2hinkle@gmail.com>
2021-03-30 02:31:47 -0400

    Implement "HostProcessor.CreateHostAndReturnIdAsync"

--- MWFDatabaseServices/MWFDataLibrary/BuisnessLogic/HostProcessor.cs
+++ MWFDatabaseServices/MWFDataLibrary/BuisnessLogic/HostProcessor.cs
@@ -4,16 +4,36 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using Dapper;
 
 namespace MWFDataLibrary.BuisnessLogic
 {
     public class HostProcessor
     {
-        public static async Task<int> CreateHostAndReturnIdAsync(string connectionString, string reqHostIp, string reqHostServicesAPISocketAddress, bool reqIsActive)
+        public static async Task<int> CreateHostAndReturnIdAsync(string connString, string hostIp, string hostServicesAPISocketAddress, bool isActive)
         {
-            
+            // name of stored procedure to execute
+            string procedureName = "spHost_CreateAndOutputId";
 
-            throw new NotImplementedException();
+
+            // create the data table representation of the user defined host table
+            DataTable hostTable = new DataTable("@inHost");
+            hostTable.Columns.Add("HostIp");
+            hostTable.Columns.Add("HostServicesAPISocketAddress");
+            hostTable.Columns.Add("IsActive", typeof(bool));
+            // fill in the data
+            hostTable.Rows.Add(hostIp, hostServicesAPISocketAddress, isActive);
+
+            // make parameters to pass to the stored procedure
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@inHost", hostTable.AsTableValuedParameter("udtHost"), DbType.Object);
+            parameters.Add("@outId", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+            // execute our stored procedure with the parameters we made
+            await SqlDataAccess.ModifyDataAsync(connString, procedureName, parameters);
+            // return the outputed id from the stored procedure
+            return parameters.Get<int>("@outId");
         }
 
         public static int RemoveHost(string serverIP, string connString)
