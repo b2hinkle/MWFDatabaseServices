afa5f473944901afe9f726226a954ca065df627d
Brian2524 <b2hinkle@gmail.com>
2021-03-30 02:19:29 -0400

    Create and implement stored procedure "spHost_CreateAndOutputId" and user defined table "udtHost"

---
+++ MWFDatabaseServices/MWFDataTools/dbo/Stored Procedures/Host/spHost_CreateAndOutputId.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spHost_CreateAndOutputId]
+	@inHost udtHost READONLY,
+	@outId int OUT	--Our out parameter so we can keep track of it in C# for when we want to destroy it
+AS
+BEGIN
+	SET NOCOUNT ON;		--Don't give how many rows affected
+
+	INSERT INTO tblHost([HostIp], [HostServicesAPISocketAddress], [IsActive])
+	SELECT [HostIp], [HostServicesAPISocketAddress], [IsActive] FROM @inHost;
+	SELECT @outId = SCOPE_IDENTITY();	--SCOPE_IDENTITY() returns the most recent modified Id within the scope of this procedure (last identity created in the same session and the same scope)
+END
+RETURN 0
